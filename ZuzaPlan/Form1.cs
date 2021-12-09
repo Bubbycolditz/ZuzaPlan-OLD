325e73fdf35f2ade1a89875e3d8cf26bbb37cee8
Bubby Colditz <Bubbycolditz@gmail.com>
2021-12-09 18:58:50 -0500

    Changelog 21.12.9
    - Initialized Project files

--- ZuzaPlan/Form1.cs
+++ ZuzaPlan/Form1.cs
@@ -16,5 +16,15 @@ namespace ZuzaPlan
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
