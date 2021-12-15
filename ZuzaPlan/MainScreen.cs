e05e5d777f1372a3fcef1fd02b6998402b331aea
Bubby Colditz <Bubbycolditz@gmail.com>
2021-12-15 15:07:11 -0500

    Changelog 21.12.15 rev4
    - Fixed unnecessary build errors
    - Refactored remaining code
    - Removed unnecessary classes & namespaces

--- ZuzaPlan/MainScreen.cs
+++ ZuzaPlan/MainScreen.cs
@@ -1,158 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ZuzaPlan
-{
-    public partial class MainScreen : Form
-    {
-        public MainScreen()
-        {
+namespace ZuzaPlan {
+    public partial class MainScreen : Form {
+        public MainScreen() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void buttonTest_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
 
         }
 
-        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
-        {
+        private void buttonTest_Click(object sender, EventArgs e) {
 
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e) {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e) {
 
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e) {
 
         }
 
-        private void notesLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
+        private void textBox1_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void Mouse_Move(object sender,MouseEventArgs e)
-        {
-            
-        }
-        private void MainScreen_MouseDown (object sender, MouseEventArgs e)
-        {
-        
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e) {
 
         }
 
-        private void notesLink_Click(object sender, EventArgs e)
-        {
+        private void notesLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
 
         }
 
-        private void projectsLink_Click(object sender, EventArgs e)
-        {
+        private void Mouse_Move(object sender, MouseEventArgs e) {
+
+        }
+        private void MainScreen_MouseDown(object sender, MouseEventArgs e) {
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
+        private void pictureBox1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void notesLink_Click(object sender, EventArgs e) {
+
+        }
+
+        private void projectsLink_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) {
             AboutScreen aboutScreen = new AboutScreen();
             aboutScreen.Show();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+        private void label1_Click(object sender, EventArgs e) {
 
         }
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            
-        }
-
-        private void resizeButton_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void exitButton_Click(object sender, EventArgs e)
-        {
+        private void panel2_Paint(object sender, PaintEventArgs e) {
 
         }
 
-        private void button1_Click_2(object sender, EventArgs e)
-        {
+        private void resizeButton_Click(object sender, EventArgs e) {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e) {
             Application.Exit();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
+        private void button2_Click(object sender, EventArgs e) {
             WindowState = FormWindowState.Minimized;
         }
 
-        private void maximizeButton_Click(object sender, EventArgs e)
-        {
-            if(WindowState == FormWindowState.Normal)
-            {
+        private void maximizeButton_Click(object sender, EventArgs e) {
+            if (WindowState == FormWindowState.Normal) {
                 WindowState = FormWindowState.Maximized;
                 maximizeButton.BackgroundImage = ZuzaPlan.Properties.Resources.Windowed;
 
-            } else 
-            {
+            } else {
                 WindowState = FormWindowState.Normal;
                 maximizeButton.BackgroundImage = ZuzaPlan.Properties.Resources.Maximize;
             }
         }
 
-        private void navbar_MouseDown(object sender, MouseEventArgs e)
-        {
+        private void navbar_MouseDown(object sender, MouseEventArgs e) {
             lastPoint = new Point(e.X, e.Y);
         }
         Point lastPoint;
-        private void navbar_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
+        private void navbar_MouseMove(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
                 this.Left += e.X - lastPoint.X;
                 this.Top += e.Y - lastPoint.Y;
             }
         }
 
-        private void signInButton_Click(object sender, EventArgs e)
-        {
+        private void signInButton_Click(object sender, EventArgs e) {
             LoginScreen signinScreen = new LoginScreen();
             signinScreen.Show();
         }
 
-        private void assignmentsButton_Click(object sender, EventArgs e)
-        {
+        private void assignmentsButton_Click(object sender, EventArgs e) {
 
         }
     }
